3c5e962755a296d58bab4a555ab0d3fd9915111a
Rei  Matsuda <rei.matsuda@imjp.co.jp>
2019-10-09 17:18:10 +0900

    とりあえずwebpack使えるようにした

--- resources/js/shaders/VertexShader.vert
+++
@@ -1,6 +0,0 @@
-precision mediump float;
-attribute vec2 vertex;
-
-void main(void) {
-  gl_Position = vec4(vertex, 0.0, 1.0);
-}
--- resources/webpack.config.js
+++ resources/webpack.config.js
@@ -32,11 +32,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: ['raw-loader']
-      },
-      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
@@ -60,6 +55,11 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: 'raw-loader'
       }
     ]
   },
